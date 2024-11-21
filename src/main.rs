3cdbc36371110758e7a0eff57953bf2be5b603cf
Nelson García Bravatti <gar22434@uvg.edu.gt>
2024-11-21 15:19:10 -0600

    Feat: Player movement and raycast implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 // main.rs
 
 use minifb::{Key, Window, WindowOptions};
-use std::{default, time::Duration};
+use std::time::Duration;
+use std::f32::consts::PI;
+use nalgebra_glm::{Vec2};
 mod framebuffer;
 use framebuffer::Framebuffer;
 mod maze;
 use maze::load_maze;
+mod player;
+use player::{Player, process_events};
+mod raycast;
+use raycast::{cast_ray};
 
 fn cell_to_color(cell: char) -> u32 {
 
@@ -32,58 +38,72 @@ fn draw_cell(framebuffer: &mut Framebuffer, xo: usize, yo: usize, block_size: us
     }   
 }
 
-fn render(framebuffer: &mut Framebuffer) {
-  let maze = load_maze("./maze.txt");
-  let block_size = 70;  // 10 pixels each block
-
-  for row in 0..maze.len() {
-    for col in 0..maze[row].len() {
-      draw_cell(framebuffer, col * block_size, row * block_size, block_size, maze[row][col]);
+fn render(framebuffer: &mut Framebuffer, player: &Player) {
+    let maze = load_maze("./maze.txt");
+    let block_size = 70;  // 100 pixels each block
+  
+    // draw the minimap
+    for row in 0..maze.len() {
+      for col in 0..maze[row].len() {
+        draw_cell(framebuffer, col * block_size, row * block_size, block_size, maze[row][col]);
+      }
     }
-  } 
-}
-
-fn main() {
-  let window_width = 900;
-  let window_height = 600;
-
-  let framebuffer_width = 900;
-  let framebuffer_height = 600;
-
-  let frame_delay = Duration::from_millis(16);
-
-  let mut framebuffer = framebuffer::Framebuffer::new(framebuffer_width, framebuffer_height);
-
-  let mut window = Window::new(
-    "Rust Graphics - Maze Example",
-    window_width,
-    window_height,
-    WindowOptions::default(),
-  ).unwrap();
-
-  // move the window around
-  window.set_position(100, 100);
-  window.update();
-
-  framebuffer.set_background_color(0x333355);
-
-  while window.is_open() {
-    // listen to inputs
-    if window.is_key_down(Key::Escape) {
-      break;
-    }
-
-    // Clear the framebuffer
-    framebuffer.clear();
-
-    // Draw some stuff
-    render(&mut framebuffer);
-
-    // Update the window with the framebuffer contents
-    window
-      .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
-      .unwrap();
-
-    std::thread::sleep(frame_delay);
+  
+    // draw the player
+    framebuffer.set_current_color(0xFFDDDD);
+    framebuffer.point(player.pos.x as usize, player.pos.y as usize);
+  
+    // draw what the player sees
+    cast_ray(framebuffer, &maze, &player, block_size);
   }
-}
+
+  fn main() {
+    let window_width = 900;
+    let window_height = 600;
+  
+    let framebuffer_width = 900;
+    let framebuffer_height = 600;
+  
+    let frame_delay = Duration::from_millis(16);
+  
+    let mut framebuffer = Framebuffer::new(framebuffer_width, framebuffer_height);
+  
+    let mut window = Window::new(
+      "Rust Graphics - Maze Example",
+      window_width,
+      window_height,
+      WindowOptions::default(),
+    ).unwrap();
+  
+    // move the window around
+    window.set_position(100, 100);
+    window.update();
+  
+    // initialize values
+    framebuffer.set_background_color(0x333355);
+    let mut player = Player {
+      pos: Vec2::new(150.0, 150.0),
+      a: PI / 3.0,
+    };
+  
+    while window.is_open() {
+      // listen to inputs
+      if window.is_key_down(Key::Escape) {
+        break;
+      }
+      process_events(&window, &mut player);
+  
+      // Clear the framebuffer
+      framebuffer.clear();
+  
+      // Draw some stuff
+      render(&mut framebuffer, &player);
+  
+      // Update the window with the framebuffer contents
+      window
+        .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
+        .unwrap();
+  
+      std::thread::sleep(frame_delay);
+    }
+  }
