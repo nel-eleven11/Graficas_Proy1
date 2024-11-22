4a82ab93eb1bc933e69a7d88a73eaaa9e0778ba5
Nelson García Bravatti <gar22434@uvg.edu.gt>
2024-11-21 20:40:24 -0600

    Feat: enemy implementation

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,1 +1,20 @@
 // enemy.rs
+
+use nalgebra_glm::Vec2;
+use once_cell::sync::Lazy;
+use std::sync::Arc;
+use crate::texture::Texture;
+
+pub static ENEMY_TEXTURE: Lazy<Arc<Texture>> = Lazy::new(|| Arc::new(Texture::new("assets/sprite1.png")));
+
+pub struct Enemy {
+    pub pos: Vec2,
+}
+
+impl Enemy {
+    pub fn new(x: f32, y: f32) -> Self {
+        Enemy {
+            pos: Vec2::new(x, y),
+        }
+    }
+}
